3d7ceb350d9df660a37e97b255274693a9cd549a
1208257 <1208257@RG43014.rgu.ac.uk>
2015-03-02 10:11:00 +0000

    Create category and subcategory classes. Small changes in db columns names for these classes.

---
+++ src/week5/CreditCardPayment.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package week5;
+
+/**
+ *
+ * @author 1208257
+ */
+public class CreditCardPayment implements PaymentBehaviour {
+
+	@Override
+	public boolean makePayment(double amount) {
+			return true; //To change body of generated methods, choose Tools | Templates.
+	}
+	
+}
